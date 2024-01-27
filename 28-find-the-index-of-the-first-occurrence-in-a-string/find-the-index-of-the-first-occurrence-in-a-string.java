93d40af6c7b69fe53e9172c0a848e0053ea115e0
Jahnavi Varaganti <116487010+Jahnavi3045@users.noreply.github.com>
2024-01-27 20:15:46 +0530

    Time: 1 ms (37.86%) | Memory: 41.5 MB (23.63%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -4,6 +4,19 @@ class Solution {
             return -1;
         }
         
-        return haystack.indexOf(needle);
+        int index=0;
+        for(int i=0;i<haystack.length();i++){
+            if(haystack.charAt(i)==needle.charAt(index)){
+                index++;
+            }
+            else{
+                i=i-index;
+                index=0;
+            }
+            if(index==needle.length()){
+                return i-needle.length()+1;
+            }
+        }
+        return -1;
     }
 }
