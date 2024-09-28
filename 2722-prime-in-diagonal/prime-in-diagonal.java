249e9867a156dc0a2cedc04d737d2c4d684ca6c7
Jahnavi Varaganti <116487010+Jahnavi3045@users.noreply.github.com>
2024-09-28 11:46:43 +0530

    Time: 15 ms (17.34%) | Memory: 59 MB (70.87%) - LeetSync

---
+++ 2722-prime-in-diagonal/prime-in-diagonal.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int diagonalPrime(int[][] nums) {
+        int m=nums.length;
+        int n=nums[0].length;
+        int maxPrime=0;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==j || i+j==n-1){
+                    if(isPrime(nums[i][j])){
+                        if(maxPrime<nums[i][j]){
+                            maxPrime=nums[i][j];
+                        }
+                    }
+                }
+            }
+        }
+
+        return maxPrime;
+
+    }
+    public boolean isPrime(int x){
+        if (x==1){
+            return false;
+        }
+        for(int i=2;i<=Math.sqrt(x);i++){
+            if(x%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
