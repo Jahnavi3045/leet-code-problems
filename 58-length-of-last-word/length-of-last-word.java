d9515bc1dce2c28557971997f0b1b66f65fc1528
Jahnavi Varaganti <116487010+Jahnavi3045@users.noreply.github.com>
2025-01-30 10:17:59 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (19.59%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,16 +1,6 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         int count=0;
-        // String str=s.strip();
-        // for(int i=0;i<str.length();i++){
-        //     if(str.charAt(i)==' '){
-        //         count =0;
-        //     }
-        //     else{
-        //         count++;
-        //     }
-        // }
-        // return count;
 
         for(int i=s.length()-1;i>=0;i--){
             if(s.charAt(i)!=' '){
