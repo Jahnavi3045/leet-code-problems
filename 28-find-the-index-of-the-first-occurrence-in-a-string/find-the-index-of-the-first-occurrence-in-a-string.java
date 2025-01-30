5cb26ae72628771303d5f82053e839568f404bab
Jahnavi Varaganti <116487010+Jahnavi3045@users.noreply.github.com>
2025-01-30 10:19:40 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (97.62%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,22 +1,24 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        if(haystack.length()<needle.length()){
-            return -1;
-        }
+        // if(haystack.length()<needle.length()){
+        //     return -1;
+        // }
         
-        int index=0;
-        for(int i=0;i<haystack.length();i++){
-            if(haystack.charAt(i)==needle.charAt(index)){
-                index++;
-            }
-            else{
-                i=i-index;
-                index=0;
-            }
-            if(index==needle.length()){
-                return i-needle.length()+1;
-            }
-        }
-        return -1;
+        // int index=0;
+        // for(int i=0;i<haystack.length();i++){
+        //     if(haystack.charAt(i)==needle.charAt(index)){
+        //         index++;
+        //     }
+        //     else{
+        //         i=i-index;
+        //         index=0;
+        //     }
+        //     if(index==needle.length()){
+        //         return i-needle.length()+1;
+        //     }
+        // }
+        // return -1;
+
+        return haystack.indexOf(needle);
     }
 }
