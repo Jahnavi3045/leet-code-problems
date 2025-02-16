193479a7e2d750756b40ef33271f2d3b117f7cfd
Jahnavi Varaganti <116487010+Jahnavi3045@users.noreply.github.com>
2025-02-16 11:07:51 +0530

    Time: 1501 ms (71.70%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,9 +1,4 @@
-with total_users as (
-    select count(*) as total from users
-)
-
-select r.contest_id,Round(count(r.user_id)*100.0/t.total,2) as percentage
+select r.contest_id,round(count(r.user_id)*100.0/(select count(*) from users),2) as percentage
 from register r
-cross join total_users t
-group by r.contest_id,t.total
+group by contest_id
 order by percentage desc,contest_id asc;
